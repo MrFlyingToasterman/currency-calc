d15d2117de860740ac10c9123cef599600bda4b8
Darius Musiolik <MrFlyingToasterman@yahoo.de>
2016-09-14 09:01:11 +0200

    added drop down menu and more currencys

--- index.php
+++ index.php
@@ -2,41 +2,48 @@
 
 <html>
     <head>
+        <!-- <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css"> -->
+
         <meta charset="UTF-8">
         <title>currency calc</title>
         <style>
             #content {
                 margin-top: 15%;
-                width: 290px;
-                height: 130px;
+                width: 350px;
+                height: 150px;
                 padding-top: 5px;
                 background-color: ghostwhite;
                 box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
             }
-            
+
             body {
                 background-color: lightgray;
             }
-            
+
             h6 {
                 margin-top: 21%;
                 color: darkslategrey;
+            }
+            
+            .boylayout {
+                width: 95%;
             }
         </style>
     </head>
     <body>
         <form action="index.php" method="post">
-            <?php     
+            <?php
             //Block warnings
             error_reporting(0);
-            //Check input from radiobox[0 or 1]
+            //Check input 
             if (isset($_POST["cb"])) {
                 //set radiboxvalue
                 $radioboxck = $_POST["cb"];
             }
-            
+
+
             //Switch case function to do calc stuff
-            switch($radioboxck) {
+            switch ($radioboxck) {
                 case 0 :
                     //if tf_euro got a value
                     if (isset($_POST["tf_euro"])) {
@@ -44,55 +51,133 @@
                         $euro = $_POST["tf_euro"];
                         //new var dollar = var euro * 1.13
                         $dollar = $euro * 1.13;
-                    }else {
+                        $var1 = " Dollar";
+                        $var0 = " Euro";
+                    } else {
                         //Leave boxes clean
                         $euro = "";
                         $dollar = "";
                     }
-                break;
-                
+                    break;
+
                 case 1 :
-                    //if tf_dollar got a value
-                    if (isset($_POST["tf_dollar"])) {
-                        //new var dollar = postvale of tf_euro
-                        $dollar = $_POST["tf_dollar"];
-                        //new var euro = var euro / 1.13
-                        $euro = $dollar / 1.13;
-                    }else {
+                    //if tf_euro got a value
+                    if (isset($_POST["tf_euro"])) {
+                        //new var euro = postvale of tf_euro
+                        $euro = $_POST["tf_euro"];
+                        //new var dollar = var euro * 1.13
+                        $dollar = $euro / 1.13;
+                        $var1 = " Euro";
+                        $var0 = " Dollar";
+                    } else {
                         //Leave boxes clean
                         $euro = "";
                         $dollar = "";
                     }
-                break;
-                
+                    break;
+                    
+                    case 2 :
+                    //if tf_euro got a value
+                    if (isset($_POST["tf_euro"])) {
+                        //new var euro = postvale of tf_euro
+                        $euro = $_POST["tf_euro"];
+                        //new var dollar = var euro * 1.13
+                        $dollar = $euro * 115.57;
+                        $var1 = " Yen";
+                        $var0 = " Euro";
+                    } else {
+                        //Leave boxes clean
+                        $euro = "";
+                        $dollar = "";
+                    }
+                    break;
+                    
+                    case 3 :
+                    //if tf_euro got a value
+                    if (isset($_POST["tf_euro"])) {
+                        //new var euro = postvale of tf_euro
+                        $euro = $_POST["tf_euro"];
+                        //new var dollar = var euro * 1.13
+                        $dollar = $euro / 115.57;
+                        $var1 = " Euro";
+                        $var0 = " Yen";
+                    } else {
+                        //Leave boxes clean
+                        $euro = "";
+                        $dollar = "";
+                    }
+                    break;
+                    
+                    case 4 :
+                    //if tf_euro got a value
+                    if (isset($_POST["tf_euro"])) {
+                        //new var euro = postvale of tf_euro
+                        $euro = $_POST["tf_euro"];
+                        //new var dollar = var euro * 1.13
+                        $dollar = $euro * 0.85;
+                        $var1 = " Pound";
+                        $var0 = " Euro";
+                    } else {
+                        //Leave boxes clean
+                        $euro = "";
+                        $dollar = "";
+                    }
+                    break;
+                    
+                    case 5 :
+                    //if tf_euro got a value
+                    if (isset($_POST["tf_euro"])) {
+                        //new var euro = postvale of tf_euro
+                        $euro = $_POST["tf_euro"];
+                        //new var dollar = var euro * 1.13
+                        $dollar = $euro / 0.85;
+                        $var1 = " Euro";
+                        $var0 = " Pound";
+                    } else {
+                        //Leave boxes clean
+                        $euro = "";
+                        $dollar = "";
+                    }
+                    break;
+
                 default:
                     //Error catch
                     echo 'Error during stuff';
-                break;
+                    break;
             }
-            
-            
-            
             ?>
             <center>
-            <div align="center" id="content">
-                
-                <table border="0px">
-                    <tr>
-                        <td>Euro:</td> <td><input type="text" name="tf_euro" value="<?php echo $euro; ?>"></td>
-                    </tr>
-                    <tr>
-                        <td>Dollar:</td> <td><input type="text" name="tf_dollar" value="<?php echo $dollar; ?>"></td>
-                    </tr>
-                    <tr>
-                        <td> &nbsp; </td> <td> &nbsp; </td>
-                    </tr>
-                    <tr>
-                        <td>€<input type="radio" checked="true" name="cb" value="0"> $<input type="radio" name="cb" value="1"></td> <td align="right"> <input type="submit"> </td>
-                    </tr>
-                </table>
-                
-            </div>
+                <div align="center" id="content">
+
+                    <table border="0px">
+                        <tr>
+                            <td colspan="2"><input type="text" placeholder="Source currency" class="boylayout" name="tf_euro" value="<?php echo round($euro, 2); echo $var0; ?>"></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><input type="text" placeholder="Converted currency" class="boylayout" name="tf_dollar" value="<?php echo round($dollar, 2); echo $var1; ?>"></td>
+                        </tr>
+                        <tr>
+                            <td> &nbsp; </td> <td> &nbsp; </td>
+                        </tr>
+                        <tr>
+                            <td> &nbsp; </td> <td> &nbsp; </td>
+                        </tr>
+                        <tr>
+                            <td width="200px">
+                                <select name="cb">
+                                    <option value="0">Euro to Dollar</option>
+                                    <option value="1">Dollar to Euro</option>
+                                    <option value="2">Euro to Yen</option>
+                                    <option value="3">Yen to Euro</option>
+                                    <option value="4">Euro to Pound</option>
+                                    <option value="5">Pound to Euro</option>
+                                </select>
+                            </td> 
+                            <td align="right"> <input type="submit"> </td>
+                        </tr>
+                    </table>
+
+                </div>
             </center>
         </form>
         <h6 align="right">PHP Hallo World by Darius Musiolik 2k16 | GPLv3 free (as in freedom) Software</h6>
